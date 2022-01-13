127117d58202f34d1ba42d3675728e598c4daf9d
Allen-dududu <xinyu.liu@perkinelmer.com>
2022-01-13 16:59:37 +0800

    migration test

---
+++ AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace TestMigration
+{
+    public class AppDbContext : DbContext
+    {
+        public const string ConnectionString = "Server=localhost;Database=postgres;username=postgres;password=postgres;";
+
+        public DbSet<Blog> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(ConnectionString);
+        }
+    }
+}
---
+++ Blog.cs
@@ -0,0 +1,10 @@
+﻿namespace TestMigration
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+    }
+}
